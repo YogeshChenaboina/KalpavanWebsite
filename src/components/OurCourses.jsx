01356168147b6444abb02145184e597acc5b51c5
yogeshchennaboina <yogeshchennaboina01@gmail.com>
2024-04-01 19:56:46 +0530

    second commit

--- src/components/OurCourses.jsx
+++ src/components/OurCourses.jsx
@@ -6,6 +6,18 @@ const OurCourses = () => {
       <div className={styles.center}>
         Our Courses
       </div>
+      <div>
+      CA is one of the toughest examinations conducted for students who want 
+      to become Chartered Accountants. CA is the dream job of many students 
+      and mostly commerce students take this examination after completing
+       hard work.
+      <br/>The CA examination is conducted in three parts and assesses the 
+      eligibility of the students for their skills to become a CA.
+      If you’re looking for institutes that provide the best coaching
+       for the toughest exam of CA, then here you are at the right place. 
+       In the following article, we reviewed some of the best institutes 
+       for CA coaching in Hyderabad.
+      </div>
     </div>
   )
 }
